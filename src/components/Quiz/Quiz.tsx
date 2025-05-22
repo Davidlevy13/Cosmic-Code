6833b958bb02ba823b5ac43f652a9b0f0c437f99
Davidlevy13 <levy.david1993@gmail.com>
2025-05-22 11:04:36 +0200

    quiz

---
+++ src/components/Quiz/Quiz.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { useState } from "react";
+import './Quiz.css'
+import { data } from "../../assets/data";
+
+const quiz = () => {
+
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index]);
+    let [lock, setLock] = useState(false);
+    let [score, setScore] = useState(0);
+    let [result, setResult] = useState(false);
+
+    let option1 = useRef(null);
+    let option2 = useRef(null);
+    let option3 = useRef(null);
+    let option4 = useRef(null);
+
+    let option_array = [option1, option2, option3, option4];
+
+    const checkAns = (e, ans) => {
+        if (lock === false) {
+            if (question.ans===ans) {
+            e.target.classList.add("correct");
+            setLock(true);
+            setScore(prev=>prev+1)
+            }
+            else{
+            e.target.classList.add("wrong");
+            setLock(true);
+            option_array[question.ans-1].current.classList.add("correct")
+            }
+        }
+        
+    }
+
+    const next = () => {
+        if (lock===true) {
+            if (index === data.length -1){
+                setResult(true);
+                return 0;
+            }
+            setIndex(++index);
+            setQuestion(data[index]);
+            setLock(false);
+            option_array.map((option) =>{
+                option.current.classList.remove("wrong");
+                option.current.classList.remove("correct");
+                return null;
+            })
+        }
+    }
+
+    const reset = () => {
+        setIndex(0);
+        setQuestion(data[0]);
+        setScore(0);
+        setLock(false);
+        setResult(false);
+    }
+
+    return(
+        <div>
+            <h1>Cosmic' Code</h1>
+            <hr/>{result?<></>:<>
+            <h2>{index+1}. {question.question}</h2>
+            <ul>
+                <li ref={option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+                <li ref={option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+                <li ref={option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+                <li ref={option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+            </ul>
+            <button type="button" onClick={next}>next</button>
+            <p>Question {index+1} sur {data.length}</p>
+            </>}
+            {result?<>
+            <h2>Votre score {score} sur {data.length}</h2>
+            <button type="button" onClick={reset}>Recommencer</button>
+            </>:<></>}
+            
+        </div>
+    )
+}
+
+export default quiz
